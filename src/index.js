f86ac2d1df0724fc90b27feae4b12f3283d5ce3f
Prabhudas-M <60952658+Prabhudas-M@users.noreply.github.com>
2020-10-26 19:11:25 -0400

    JSX-Practice - 1

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>My Favarite Places</h1>
+    <ul>
+      <li>Munnar</li>
+      <li>Kodaikkanal</li>
+      <li>Bangalore</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
